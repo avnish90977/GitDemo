8efb81e3a937048bd81903acca067fbed7b6e83a
Avnish <yadavavnishraj@gmail.com>
2024-01-16 00:37:26 +0530

    Architature changes From US

--- 1st.py
+++ 1st.py
@@ -7,3 +7,14 @@ print(type(a))
 print(type(c))
 print(type(b))
 
+#C:\Users\yadav\1 Comntributer>git pull origin master
+remote: Enumerating objects: 7, done.
+remote: Counting objects: 100% (7/7), done.
+remote: Compressing objects: 100% (1/1), done.
+remote: Total 4 (delta 3), reused 4 (delta 3), pack-reused 0
+Unpacking objects: 100% (4/4), 385 bytes | 19.00 KiB/s, done.
+From https://github.com/avnish90977/GitDemo
+ * branch            master     -> FETCH_HEAD
+   2dd2919..37d0f4e  master     -> origin/master
+Updating 2dd2919..37d0f4e
+Fast-forward
